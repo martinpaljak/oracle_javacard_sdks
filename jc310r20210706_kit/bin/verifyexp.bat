5f0d93cac17c51b610401ac34d1f065470aabc95
Martin Paljak <martin@martinpaljak.net>
2021-12-31 09:35:00 +0200

    Import 3.1.0 version of 2021-07-06
    $ shasum -a 256 java_card_tools-win-bin-b_17-06_jul_2021.zip
    b990d1c8b3eab2224f1f4b1e2cff301c0b7bb939d26a114fdfd9f08d550093f1  java_card_tools-win-bin-b_17-06_jul_2021.zip

---
+++ jc310r20210706_kit/bin/verifyexp.bat
@@ -0,0 +1,42 @@
+
+@echo off
+
+@REM Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
+
+setlocal
+set JC_HOME_TOOLS=%~dp0\..
+rem Print warning if no JAVA_HOME set
+if not defined JAVA_HOME goto nojavahome
+set JC_LIBS=%JC_HOME_TOOLS%
+
+
+rem set classpath to all jars
+
+set JC_CLASSPATH=%JC_LIBS%\lib\asm-8.0.1.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\commons-cli-1.4.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\commons-logging-1.2-9f99a00.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\jctasks_tools.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\json.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\tools.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\api_classic-3.1.0.jar;%JC_CLASSPATH%
+
+set JC_CLASSPATH=%JC_LIBS%\lib\api_classic_annotations-3.1.0.jar;%JC_CLASSPATH%
+
+rem execute verifyexp's Main class
+"%JAVA_HOME%\bin\java" "-Djc.home=%JC_HOME_TOOLS%" -classpath "%JC_CLASSPATH%" com.sun.javacard.offcardverifier.VerifyExp %*
+goto done
+:nojavahome
+echo JAVA_HOME is not set. Please set it to point to JDK 7 or JDK 8
+
+:done
+endlocal
+
+rem Send the error code to the command interpreter
+cmd /c Exit /B %errorlevel%
+        
