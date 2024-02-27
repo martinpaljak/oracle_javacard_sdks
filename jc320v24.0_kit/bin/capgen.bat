59458b555e29c0ceca0b86e7f707dc87d6e9561c
Martin Paljak <martin@martinpaljak.net>
2024-02-27 20:08:29 +0200

    Import 3.2.0 b57 (2024-02-20)
    $ shasum -a 256 java_card_devkit_tools-bin-v24.0-b_57-20-FEB-2024.zip
    94eaa387f060a1a10dc159c50b31a1520893181ae234cc6ab72a7823747cc2b0  java_card_devkit_tools-bin-v24.0-b_57-20-FEB-2024.zip

---
+++ jc320v24.0_kit/bin/capgen.bat
@@ -0,0 +1,42 @@
+
+@echo off
+
+@REM Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
+
+setlocal
+set JC_HOME_TOOLS=%~dp0\..
+rem Print warning if no JAVA_HOME set
+if not defined JAVA_HOME goto nojavahome
+set JC_LIBS=%JC_HOME_TOOLS%
+
+
+rem set classpath to all jars
+
+set JC_CLASSPATH=%JC_LIBS%\lib\asm-9.4.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\commons-cli-1.6.0.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\commons-logging-1.3.0.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\jctasks_tools.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\json.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\tools.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\api_classic-3.2.0.jar;%JC_CLASSPATH%
+
+set JC_CLASSPATH=%JC_LIBS%\lib\api_classic_annotations-3.2.0.jar;%JC_CLASSPATH%
+
+rem execute capgen's Main class
+"%JAVA_HOME%\bin\java" "-Djc.home=%JC_HOME_TOOLS%" -classpath "%JC_CLASSPATH%" com.sun.javacard.jcasm.cap.Main %*
+goto done
+:nojavahome
+echo JAVA_HOME is not set. Please set it to point to JDK 17
+
+:done
+endlocal
+
+rem Send the error code to the command interpreter
+cmd /c Exit /B %errorlevel%
+        
---
+++ jc320v24.0_kit/bin/capgen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
+
+# Print warning if no JAVA_HOME set
+if [ -z "${JAVA_HOME}" ]; then
+	echo "JAVA_HOME is not set. Please set it to point to JDK 17"
+	exit 1
+fi
+
+JC_LIBS=$(dirname $(realpath $0))/../lib
+
+# set classpath to all jars
+JC_CLASSPATH=$JC_LIBS/asm-9.4.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/commons-cli-1.6.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/commons-logging-1.3.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/jctasks_tools.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/json.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/tools.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/api_classic-3.2.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/api_classic_annotations-3.2.0.jar:$JC_CLASSPATH
+
+# execute capgen's Main class
+$JAVA_HOME/bin/java -cp ".:${JC_CLASSPATH}" com.sun.javacard.jcasm.cap.Main "$@"
+
