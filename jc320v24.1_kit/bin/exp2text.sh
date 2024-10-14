e2df471e04d86f33de69a947f44766fbef1d9d69
Martin Paljak <martin@martinpaljak.net>
2024-10-14 12:34:32 +0300

    Import 3.2.0 u1 / 24.1 (2024-10-06)
    $ shasum -a 256 java_card_devkit_tools-bin-v24.1-b_284-06-OCT-2024.zip
    3f940a260961a723d3f834778a912ec1383a3ee46834b8c3cb308dab193c3e7e  java_card_devkit_tools-bin-v24.1-b_284-06-OCT-2024.zip

---
+++ jc320v24.1_kit/bin/exp2text.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
+
+# Print warning if no JAVA_HOME set
+if [ -z "${JAVA_HOME}" ]; then
+	echo "JAVA_HOME is not set. Please set it to point to JDK 17"
+	exit 1
+fi
+
+JC_LIBS=$(dirname $(realpath $0))/../lib
+
+# set classpath to all jars
+JC_CLASSPATH=$JC_LIBS/asm-9.6.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/commons-cli-1.6.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/commons-logging-1.3.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/jctasks_tools.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/json.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/tools.jar:$JC_CLASSPATH
+
+# execute exp2text's Main class
+$JAVA_HOME/bin/java -cp ".:${JC_CLASSPATH}" com.sun.javacard.converter.Exp2Text "$@"
+
