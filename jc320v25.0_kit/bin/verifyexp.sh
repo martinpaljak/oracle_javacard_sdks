7cad0e6870977a8ba39851eb64c7941bce1ddb23
Martin Paljak <martin@martinpaljak.net>
2025-05-05 21:58:45 +0300

    Import 3.2.0 / 25.0 (2025-04-23)
    $ shasum -a 256 java_card_devkit_tools-bin-v25.0-b_470-23-APR-2025.zip
    5150839b9d5289e2ab154757d9ea21092af030abbc81cd50619cd462d595c8fb  java_card_devkit_tools-bin-v25.0-b_470-23-APR-2025.zip

---
+++ jc320v25.0_kit/bin/verifyexp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Copyright (c) 1998, 2025, Oracle and/or its affiliates. All rights reserved.
+
+# Print warning if no JAVA_HOME set
+if [ -z "${JAVA_HOME}" ]; then
+	echo "JAVA_HOME is not set. Please set it to point to JDK 17"
+	exit 1
+fi
+
+JC_LIBS=$(dirname $(realpath $0))/../lib
+
+# set classpath to all jars
+JC_CLASSPATH=$JC_LIBS/commons-cli-1.9.0.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/jctasks_tools.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/json.jar:$JC_CLASSPATH
+JC_CLASSPATH=$JC_LIBS/tools.jar:$JC_CLASSPATH
+
+# execute verifyexp's Main class
+$JAVA_HOME/bin/java -cp ".:${JC_CLASSPATH}" com.sun.javacard.offcardverifier.VerifyExp "$@"
+
---
+++ jc320v25.0_kit/bin/verifyrev.bat
@@ -0,0 +1,35 @@
+
+@echo off
+
+@REM Copyright (c) 1998, 2025, Oracle and/or its affiliates. All rights reserved.
+
+setlocal
+set JC_HOME_TOOLS=%~dp0\..
+rem Print warning if no JAVA_HOME set
+if not defined JAVA_HOME goto nojavahome
+set JC_LIBS=%JC_HOME_TOOLS%
+
+
+rem set classpath to all jars
+
+set JC_CLASSPATH=%JC_LIBS%\lib\commons-cli-1.9.0.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\jctasks_tools.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\json.jar;%JC_CLASSPATH%
+                
+set JC_CLASSPATH=%JC_LIBS%\lib\tools.jar;%JC_CLASSPATH%
+                
+
+rem execute verifyrev's Main class
+"%JAVA_HOME%\bin\java" "-Djc.home=%JC_HOME_TOOLS%" -classpath "%JC_CLASSPATH%" com.sun.javacard.offcardverifier.VerifyRev %*
+goto done
+:nojavahome
+echo JAVA_HOME is not set. Please set it to point to JDK 17
+
+:done
+endlocal
+
+rem Send the error code to the command interpreter
+cmd /c Exit /B %errorlevel%
+        
